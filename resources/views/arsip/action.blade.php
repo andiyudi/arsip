7a94baae499b273a1d3b1dbda3191168642b8324
andiyudi <49513145+andiyudi@users.noreply.github.com>
2023-02-17 14:08:05 +0700

    make pop up delete data

--- resources/views/arsip/action.blade.php
+++ resources/views/arsip/action.blade.php
@@ -1,7 +1,7 @@
  <div class="table-actions">
     <a href="{{ route($route.'.edit', $data->id) }}" class="btn btn-sm btn-outline-warning action"><i class="ti-pencil"></i></a>
     <a href="{{ route($route.'.show', $data->id) }}" class="btn btn-sm btn-outline-info action"><i class="ti-info"></i></a>
-    <form action="{{ route($route.'.destroy', $data->id) }}" method="POST" style="display: inline-block;">
+    <form action="{{ route($route.'.destroy', $data->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this data?')" style="display: inline-block;">
         @csrf
         @method('DELETE')
         <button type="submit" class="btn btn-sm btn-outline-danger action"><i class="ti-trash"></i></button>
